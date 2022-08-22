524b747819ff5bd553f5e968907ad7ead59016f2
Rbrogniez <5013@holbertonschool.com>
2022-08-22 16:01:18 +0200

    Add all files for task 0

---
+++ sort.h
@@ -0,0 +1,26 @@
+#ifndef HEADER
+#define HEADER
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+
+
+void insertion_sort_list(listint_t **list);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void swap(int* xp, int* yp);
+void bubble_sort(int arr[], int n);
+
+#endif /* HEADER */
