5dcfd82686c4a36c5cdb1212019129aa9382af8e
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 <l.leroty@gmail.com>
2022-08-22 16:47:25 +0200

    fix betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,26 +2,38 @@
 #include <stdlib.h>
 #include "sort.h"
 
-void swap(int* xp, int* yp)
+/**
+ * swap - A function that swaps two elements
+ * @xp: first element to swap
+ * @yp: second element to swap
+ * Return: void
+ */
+void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
 }
 
+/**
+ * bubble_sort - A function that sorts an array of integers in ascending
+ * order using the Bubble sort algorithm
+ * @array: the array we want to print
+ * @size: the number of element in the array
+ */
 void bubble_sort(int *array, size_t size)
 
 {
-    long unsigned int i, j;
-    for (i = 0; i < size - 1; i++)
+unsigned long int i, j;
+for (i = 0; i < size - 1; i++)
 
 
-        for (j = 0; j < size - i - 1; j++)
-            if (array[j] > array[j + 1])
-                {
-                swap(&array[j], &array[j + 1]);
-                print_array (array, size);
-                }
+for (j = 0; j < size - i - 1; j++)
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+}
 }
 
 
--- sort.h
+++ sort.h
@@ -10,9 +10,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 
 
@@ -20,7 +20,7 @@ typedef struct listint_s
 void insertion_sort_list(listint_t **list);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void swap(int* xp, int* yp);
+void swap(int *xp, int *yp);
 void bubble_sort(int *array, size_t size);
 
 #endif /* HEADER */
