72f38d2cf635f60f78ace0a0e7f8ebd21a6fcea9
Rbrogniez <5013@holbertonschool.com>
2022-08-23 14:47:52 +0200

    betty fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include "sort.h"
 
+/**
+ * swap - A function that swaps two elements
+ * @xp: first element to swap
+ * @yp: second element to swap
+ * Return: void
+ */
+
 void swap(int *xp, int *yp)
 {
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
 }
+
+/**
+ * selection_sort - function that sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: the array we want to print
+ * @size: the number of element in the array
+ */
 
 void selection_sort(int *array, size_t size)
 
@@ -14,19 +28,20 @@ void selection_sort(int *array, size_t size)
 	unsigned long int i, j, main_index;
 
 
-    for (i = 0; i < size-1; i++)
-    {
+for (i = 0; i < size - 1; i++)
+{
 
-        main_index = i;
-        for (j = i+1; j < size; j++)
-          if (array[j] < array[main_index])
-            main_index = j;
+main_index = i;
+
+for (j = i + 1; j < size; j++)
+if (array[j] < array[main_index])
+main_index = j;
 
 
-        if(main_index != i)
-        {
-            swap(&array[main_index], &array[i]);
-            print_array(array, size);
-        }
-    }
+if (main_index != i)
+{
+swap(&array[main_index], &array[i]);
+print_array(array, size);
+}
+}
 }
